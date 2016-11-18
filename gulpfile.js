3364a27cce8d8630a36d0374986300ce4575c5d3
anewryzm <anewryzm@gmail.com>
2016-11-18 18:56:11 -0500

    sass task for all files ending with .scss in app/scss and children dirs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
+  // Get all files ending with .scss
+  // in app/scss and children dirs
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
 })
