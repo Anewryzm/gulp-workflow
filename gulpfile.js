97e16a12bad650da3d820a9fd1908ec623147168
anewryzm <anewryzm@gmail.com>
2016-11-18 19:05:58 -0500

    error prevention for watching sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
   // Get all files ending with .scss
   // in app/scss and children dirs
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass())
+  // Listen for errors in sass()
+    .pipe(sass().on('error', errorHandler))
     .pipe(gulp.dest('app/css'))
 })
+
+function errorHandler(err){
+  console.log(err.toString());
+  this.emit('end');
+}
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
